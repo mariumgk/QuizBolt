35378e70cdeaa68d92cd5e7d7d277f9ec72637af
Marium Imran <mariumgk@example.com>
2026-05-08 16:36:57 +0500

    Remove test-embed directory to fix build errors

--- app/test-embed/page.tsx
+++
@@ -1,9 +0,0 @@
-import { embedChunksAction } from "../actions/embed-local";
-
-export default async function TestEmbedPage() {
-  const result = await embedChunksAction([
-    "The quick brown fox jumps over the lazy dog.",
-  ]);
-
-  return <pre>{JSON.stringify(result, null, 2)}</pre>;
-}
